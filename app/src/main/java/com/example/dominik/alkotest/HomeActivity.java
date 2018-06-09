1dafb9591e287c4cb138f6793335ff65e5e9fcd9
ksenat <senat.karol@gmail.com>
2018-06-09 20:38:47 +0200

    front

---
+++ app/src/main/java/com/example/dominik/alkotest/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.dominik.alkotest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void rejestruj (View view){
+
+        android.content.Intent myIntent = new android.content.Intent(view.getContext(), Rejestruj.class);
+        startActivity(myIntent);
+
+
+    }
+
+    public void loguj (View view){
+
+        android.content.Intent myIntent = new android.content.Intent(view.getContext(), TestPo.class);
+        startActivity(myIntent);
+
+    }
+}
